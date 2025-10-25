26ef72db3ad1ae57d723bba2d9359c7107167329
khaelano <khaelanoabroor@gmail.com>
2025-10-25 09:29:50 +0700

    Implements comprof section

--- components/sections/Comprof.tsx
+++ components/sections/Comprof.tsx
@@ -1,3 +1,25 @@
+import VideoPlayer from "../VideoPlayer";
+
 export default function ComprofSection() {
-    return (<h1>Company profile section Section</h1>)
+  return (
+    <section className="section-bg bg-agro-green-600 text-agro-green-100">
+      <div className="section-content py-16">
+        <div id="comprof-header" className="flex flex-col lg:flex-row gap-4">
+          <h2 className="flex flex-1 font-heading font-bold text-4xl tracking-tight">
+            Solusi Cerdas untuk Pertanian Indonesia yang Lebih Sehat dan
+            Berkelanjutan
+          </h2>
+          <p className="flex flex-1">
+            Agroplus memberikan solusi cerdas bagi petani dengan menghadirkan
+            inovasi berbasis teknologi yang membantu petani mengelola tanaman,
+            sumber daya, dan hasil panen secara lebih efisien. Agroplus berupaya
+            menciptakan ekosistem pertanian yang tidak hanya produktif, tetapi
+            juga ramah lingkungan dan mampu mendukung kesejahteraan petani dalam
+            jangka panjang.
+          </p>
+        </div>
+      </div>
+      <VideoPlayer src="/videos/comprof-landscape.mp4" loop></VideoPlayer>
+    </section>
+  );
 }
