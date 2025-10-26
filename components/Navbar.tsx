5318789765cf3058c71bf04de929c9f4f3770fa9
khaelano <khaelanoabroor@gmail.com>
2025-10-27 02:26:49 +0700

    Make Navbar background adaptive

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,15 +1,26 @@
-"use client"
+"use client";
 
 import Image from "next/image";
 import LogoMono from "./icons/LogoMono";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
 export default function Navbar() {
-  let [transparent, setTransparent] = useState(false);
+  const [transparent, setTransparent] = useState(true);
+
+  useEffect(() => {
+    const scrollHandler = () => {
+      setTransparent(window.scrollY <= 0);
+    };
+
+    window.addEventListener("scroll", scrollHandler);
+    return () => window.removeEventListener("scroll", scrollHandler);
+  }, []);
 
   return (
-    <div className={`flex w-full px-6 py-8 justify-center ${!transparent ? "bg-primary-1 text-primary-2" : "text-primary-1"}`}>
-      <div className="flex w-full max-w-7xl">
+    <section
+      className={`fixed section-bg h-20 place-content-center z-50 transition-all duration-150 ${!transparent ? "bg-primary-1 text-primary-2" : "text-primary-1"}`}
+    >
+      <div className="flex section-content">
         <div id="logo" className="space gap-2 flex flex-row">
           <LogoMono className={`size-7`} />
           <h1>
@@ -17,14 +28,17 @@ export default function Navbar() {
             <span className="font-normal italic">PLUS</span>
           </h1>
         </div>
-        <div id="navigation" className="flex flex-grow justify-end flex-row gap-9">
-            <a href="#">Tentang Kami</a>
-            <a href="#">Layanan Kami</a>
-            <a href="#">Mitra Kami</a>
-            <a href="#">Tim Kami</a>
-            <a href="#">FAQ</a>
+        <div
+          id="navigation"
+          className="flex flex-grow justify-end flex-row gap-9"
+        >
+          <a href="#">Tentang Kami</a>
+          <a href="#">Layanan Kami</a>
+          <a href="#">Mitra Kami</a>
+          <a href="#">Tim Kami</a>
+          <a href="#">FAQ</a>
         </div>
       </div>
-    </div>
+    </section>
   );
 }
