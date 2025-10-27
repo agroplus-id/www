dd7a2d6001e1d64b3313bbed7712f60c7a3f670d
khaelano <khaelanoabroor@gmail.com>
2025-10-27 07:25:12 +0700

    Set correct favicon

--- app/layout.tsx
+++ app/layout.tsx
@@ -26,6 +26,9 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <head>
+        <meta name="apple-mobile-web-app-title" content="Agroplus" />
+      </head>
       <body className={`${dmSans.variable} ${fjGrotesk.variable} antialiased`}>
         <Navbar />
         {children}
