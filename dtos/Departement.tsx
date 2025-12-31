663d40e2f7a163d0cc7724466d79d250ca12e96e
onyourjie <filzahmufidah@student.ub.ac.id>
2025-12-31 12:26:50 +0700

    jir lupa

--- dtos/Departement.tsx
+++ dtos/Departement.tsx
@@ -3,4 +3,14 @@ type DeptData = {
   title: string;
   description: string;
   gradient: [string, string];
+  metrics?: {
+    left: {
+      value: string;
+      label: string;
+    };
+    right: {
+      value: string;
+      label: string;
+    };
+  };
 };
